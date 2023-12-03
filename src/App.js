9e51d91bfaf7d017c92aec3f41411f8139591620
divyaac1 <divyakgl6366@gmail.com>
2023-12-03 19:46:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import ToDoList from './Components/ToDoList'
+import Navigation from './Navigation/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navigation/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contacts" element={<Contact/>} />
+          <Route path="/todolist" element={<ToDoList/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
